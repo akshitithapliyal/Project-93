e26046559665c6c04ee89e36755aecf655aaf582
Akshiti <100438051+akshitithapliyal@users.noreply.github.com>
2022-04-26 18:04:05 +0200

    Add files via upload

---
+++ letschat_page.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApg0ff18pb-tkLjUEPB7aPvo5jVG6SKds",
+    authDomain: "akshiti-kwitter-page.firebaseapp.com",
+    databaseURL: "https://akshiti-kwitter-page-default-rtdb.firebaseio.com",
+    projectId: "akshiti-kwitter-page",
+    storageBucket: "akshiti-kwitter-page.appspot.com",
+    messagingSenderId: "736729335492",
+    appId: "1:736729335492:web:6ec234a317009ff33a6b91"
+    };
+    firebase.initializeApp(firebaseConfig);
